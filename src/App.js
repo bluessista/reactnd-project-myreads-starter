81834c01a82876bbab02e40c01356183abf401b9
bluessista <inga@bluessista.com>
2018-07-29 17:01:16 +0200

    use BooksAPI to getAll books
    declare shelfs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import { Route } from 'react-router-dom'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
+    shelfs: [
+      {
+        name: 'Currently Reading',
+        shelf: 'currentlyReading',
+      },
+      {
+        name: 'Want to Read',
+        shelf: 'wantToRead',
+      },
+      {
+        name: 'Read',
+        shelf: 'read',
+      }
+    ],
+    books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      books: this.setState({ books })
+      console.log(this.state.books)
+    })
+  }
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <SearchBooks />
+        <Route path="/search" render={() => (<SearchBooks />)} />
+        <Route path="/" exact render={() => (
+          <ListBooks shelfs={this.state.shelfs} books={this.state.books}/>
         )}
         />
-        <Route path="/" exact render={() => (
-          <ListBooks />
-        )}/>
       </div>
     )
   }
