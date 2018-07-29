bf8e24923b2e82e3dcbee5241a10bfc967ee4ebb
bluessista <inga@bluessista.com>
2018-07-29 17:02:52 +0200

    render Books in Shelf

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,14 +4,20 @@ import Book from './Book'
 class Shelf extends Component {
     render () {
         return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">New Shelf</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        <li>
-                            <Book />
-                        </li>
-                    </ol>
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">{this.props.shelf.name}</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                        {this.props.books
+                            .filter(book => book.shelf === this.props.shelf.shelf)
+                            .map(book => 
+                            <li key={book.id}>
+                                <Book book={book}/>
+                            </li>
+                        )}
+                        </ol>
+                    </div>
                 </div>
             </div>
         )
